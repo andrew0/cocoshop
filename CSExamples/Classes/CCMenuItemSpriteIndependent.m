39865b8ce9e5fb8c222b91c2c150b72dd975d17d
Stepan Generalov <psi.pk.ru@gmail.com>
2011-05-22 21:42:42 +0400

    CleanUp, Comment & License CCMenuItemSpriteIndependent

--- CSExamples/Classes/CCMenuItemSpriteIndependent.m
+++ CSExamples/Classes/CCMenuItemSpriteIndependent.m
@@ -1,77 +1,79 @@
-//
-//  CCMenuItemSpriteIndependent.m
-//  CCMenuAdvanced
-//
-//  Created by Stepan Generalov on 16.11.10.
-//  Copyright 2010 Parkour Games. All rights reserved.
-//
+/*
+ * CCMenuItemSpriteIndependent.m
+ * cocos2d extensions
+ *
+ * Copyright (c) 2010-2011 Stepan Generalov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 #import "CCMenuItemSpriteIndependent.h"
 
 @implementation CCMenuItemSpriteIndependent
 
 
+// returns only contentSize in rect.size, cause CCMenu doesn't use origin anyway
+// contentSize_ is set in init with normalImage_ content size
 -(CGRect) rect
 {
 	return CGRectMake( 0, 0, contentSize_.width, contentSize_.height );	
 }
 
+// delegates point conversion to normalImage_
 - (CGPoint)convertToNodeSpace:(CGPoint)worldPoint
 {
 	return [normalImage_ convertToNodeSpace: worldPoint];
 }
 
+// retains normal image, doesnt add it as a child
 -(void) setNormalImage:(CCNode <CCRGBAProtocol>*)image
 {
-	if( image != normalImage_ ) {
-		//
+	if( image != normalImage_ ) 
+	{
 		[normalImage_ release];
-		
-		//image.anchorPoint = ccp(0,0);
 		image.visible = YES;
-		
-		//[self removeChild:normalImage_ cleanup:YES];
-		//[self addChild:image];
-		
-		//normalImage_ = image;
 		normalImage_ = [image retain];
 	}
 }
 
+// retains selected image, doesnt add it as a child
 -(void) setSelectedImage:(CCNode <CCRGBAProtocol>*)image
 {
-	if( image != selectedImage_ ) {
-		//
+	if( image != selectedImage_ ) 
+	{
 		[selectedImage_ release];
-		
-		//image.anchorPoint = ccp(0,0);
 		image.visible = NO;
-		
-		//[self removeChild:selectedImage_ cleanup:YES];
-		//[self addChild:image];
-		
-		//selectedImage_ = image;
 		selectedImage_ = [image retain];
 	}
 }
 
+// retains disabled image, doesnt add it as a child
 -(void) setDisabledImage:(CCNode <CCRGBAProtocol>*)image
 {
-	if( image != disabledImage_ ) {
-		//
+	if( image != disabledImage_ ) 
+	{
 		[disabledImage_ release];
-		
-		//image.anchorPoint = ccp(0,0);
 		image.visible = NO;
-		
-		//[self removeChild:disabledImage_ cleanup:YES];
-		//[self addChild:image];
-		
-		//disabledImage_ = image;
 		disabledImage_ = [image retain];
 	}
 }
-
 
 - (void) dealloc
 {
